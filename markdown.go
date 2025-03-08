b5512edaf0fc17cadd441a394ddabe4ff4bcbe0f
Jacob Dearing <jacob.dearing@gmail.com>
2025-03-08 10:48:09 +0000

    add easy heading and horizontal rule

--- markdown.go
+++ markdown.go
@@ -1,12 +1,59 @@
 package markdown
 
+import "strings"
+
 type Markdown struct{}
+
+const (
+	parseError = iota
+	parseHeading
+	parseHorizontalRule
+	parseUnorderedList
+)
 
 func New() Markdown {
 	return Markdown{}
 }
 
 func (m Markdown) MarkdownToHTML(markdown string) (string, error) {
-	// This is a placeholder function
-	return markdown, nil
+
+	html := ""
+
+	for line := range strings.Lines(markdown) {
+
+		// headings <h1> to <h6>
+		if strings.HasPrefix(line, "# ") {
+			html += "<h1>" + strings.TrimSpace(line[2:]) + "</h1>"
+			continue
+		}
+		if strings.HasPrefix(line, "## ") {
+			html += "<h2>" + strings.TrimSpace(line[3:]) + "</h2>"
+			continue
+		}
+		if strings.HasPrefix(line, "### ") {
+			html += "<h3>" + strings.TrimSpace(line[4:]) + "</h3>"
+			continue
+		}
+		if strings.HasPrefix(line, "#### ") {
+			html += "<h4>" + strings.TrimSpace(line[5:]) + "</h4>"
+			continue
+		}
+		if strings.HasPrefix(line, "##### ") {
+			html += "<h5>" + strings.TrimSpace(line[6:]) + "</h5>"
+			continue
+		}
+		if strings.HasPrefix(line, "###### ") {
+			html += "<h6>" + strings.TrimSpace(line[7:]) + "</h6>"
+			continue
+		}
+
+		// horizontal rule <hr/>
+		if strings.HasPrefix(line, "---") {
+			html += "<hr/>"
+			continue
+		}
+
+	}
+
+	return html, nil
 }
--- markdown_test.go
+++ markdown_test.go
@@ -1,12 +1,31 @@
 package markdown
 
-import "testing"
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
 
-func TestMarkdownH1(t *testing.T) {
+func TestMarkdownHeading(t *testing.T) {
+	for i := range 5 {
+		test := strings.Repeat("#", i+1) + " Hello World"
+		expected := fmt.Sprintf("<h%d>Hello World</h%d>", i+1, i+1)
+		md := New()
 
-	test := "# Hello World"
-	expected := "<h1>Hello World</h1>"
+		result, err := md.MarkdownToHTML(test)
+		if err != nil {
+			t.Errorf("Error: %s", err)
+		}
 
+		if result != expected {
+			t.Errorf("Expected: %s, Got: %s", expected, result)
+		}
+	}
+}
+
+func TestMarkdownHorizontalRule(t *testing.T) {
+	test := "---"
+	expected := "<hr/>"
 	md := New()
 
 	result, err := md.MarkdownToHTML(test)
